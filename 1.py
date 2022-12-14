50d51b3724efdd9ebc829b21519db8682c07045a
Maxim Fursov <maxick.95@mail.ru>
2022-12-14 10:41:39 +0300

    First commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+"""
+Поработайте с переменными, создайте несколько,
+выведите на экран, запросите у пользователя несколько чисел и
+строк и сохраните в переменные, выведите на экран.
+"""
+user_name = input("Vvedite imya: ")
+sur_name = input("Vvedite familiyu: ")
+passwd = input("Vvedite vash parol: ")
+age = input("Vvedite vash vozrast: ")
+print(f"Vash dannye: Imya - {user_name}, Familya - {sur_name},"
+      f" Vozrast - {age}, Parol - {passwd}")
+""""""
